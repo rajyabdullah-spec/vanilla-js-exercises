0bd66b41cd1f796a71c3c0a2551f31ceb68aad40
rajyabdullah-spec <rajyabdullah@gmail.com>
2026-05-09 22:21:00 +0200

    Refactor car object with methods for starting and driving

--- 05-Objects-as-Classes/script.js
+++ 05-Objects-as-Classes/script.js
@@ -1,25 +1,17 @@
-/* 
-  Path: vanilla-js-exercises/05-Objects-as-Classes/main.js
-*/
-
-// 1. إنشاء كائن يمثل "Class" للسيارة مع الخصائص والميثود الأساسي
 const car = {
   make: "Nissan",
   model: "Skyline GT-R R34",
   year: 1999,
   
-  // ميثود لتشغيل السيارة
   start: function() {
     console.log(`The ${this.make} ${this.model} is starting... Vroom Vroom! 🏎️`);
   },
 
-  // 2. تمديد الكائن بميثود للقيادة (Drive)
   drive: function() {
     console.log(`The ${this.year} ${this.model} is now driving down the road.`);
   }
 };
 
-// اختبار الميثودز وطباعة الرسائل
 console.log("--- Car Instance Test ---");
 car.start(); // استدعاء ميثود التشغيل
 car.drive(); // استدعاء ميثود القيادة

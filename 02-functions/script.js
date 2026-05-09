6585f8aa92296b183c003fdf0bc0464557bfd5d9
rajyabdullah-spec <rajyabdullah@gmail.com>
2026-05-09 22:18:56 +0200

    Refactor reverseString to use arrow function syntax

--- 02-functions/script.js
+++ 02-functions/script.js
@@ -1,19 +1,18 @@
-// Part 1: Sum Function
+
 function calculateSum(num1, num2) {
   return num1 + num2;
 }
 
-// Testing the function
+
 console.log(calculateSum(5, 10));  // Output: 15
 console.log(calculateSum(22, 8));  // Output: 30
 
 
-// Part 2: Reverse String Function
-// سنستخدم الـ Arrow Function هنا لتظهر احترافيتك (Refactoring)
+
 const reverseString = (text) => {
   return text.split("").reverse().join("");
 };
 
-// Testing the function
+
 console.log(reverseString("hello")); // Output: "olleh"
 console.log(reverseString("Raji"));  // Output: "ijaR"

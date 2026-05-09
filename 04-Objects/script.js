68f7815c148eb21e1326ecb11a5e113bdbf6ad4e
rajyabdullah-spec <rajyabdullah@gmail.com>
2026-05-09 22:20:21 +0200

    Refactor script.js to improve readability
    Removed unnecessary comments and cleaned up the code.

--- 04-Objects/script.js
+++ 04-Objects/script.js
@@ -1,30 +1,21 @@
-/* 
-  Path: vanilla-js-exercises/04-Objects/ٍscript.js
-*/
-
-// 1. إنشاء كائن يمثل كتاباً
 const book = {
   title: "To Kill a Mockingbird",
   author: "Harper Lee",
   year: 1960
 };
 
-// طباعة تفاصيل الكتاب
 console.log("--- Book Information ---");
 console.log(`Title: ${book.title}`);
 console.log(`Author: ${book.author}`);
 console.log(`Year: ${book.year}`);
 
-// ---
 
-// 2. تعريف كائن يمثل شخصاً
 const person = {
   name: "Raji",
   age: 30,
   gender: "Male"
 };
 
-// دالة تستقبل كائن الشخص كمعامل (Parameter)
 function printPersonDetails(individual) {
   console.log("\n--- Person Information ---");
   console.log(`Name: ${individual.name}`);
@@ -32,5 +23,4 @@ function printPersonDetails(individual) {
   console.log(`Gender: ${individual.gender}`);
 }
 
-// استدعاء الدالة وتمرير كائن person لها
 printPersonDetails(person);
